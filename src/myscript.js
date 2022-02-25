0b8aa9be43b9124f6559d188591b1b62de11a5d6
Fedele0 <98784738+Fedele0@users.noreply.github.com>
2022-02-26 00:18:00 +0100

    Update myscript.js

--- src/myscript.js
+++ src/myscript.js
@@ -3,3 +3,6 @@ console.log('Gioco del tris');
 let play = function(r,c) {
     console.log(`Hai premuto sulla casella: ${r} ${c}`);
 }
+let menu = function(r,c) {
+    console.log(`Hai premuto il menu`);
+}
